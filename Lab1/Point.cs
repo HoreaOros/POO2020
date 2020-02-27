e6f8e74906cea4d5affbd2baa28b6ab319a10ec7
Horea Oros <horea.oros@gmail.com>
2020-02-27 09:32:52 +0200

    add lab1

---
+++ Lab1/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Point
+    {
+        private double x, y;
+
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            Console.WriteLine("Am creat un obiect de tip Point({0}, {1})", x, y);
+        }
+
+        public double DistanceToOrigin()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public double DistanceTo(Point p2)
+        {
+            return Math.Sqrt(Math.Pow(x - p2.x, 2) + Math.Pow(y - p2.y, 2));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        public void MoveTo(double x, double y)
+        {
+            Console.WriteLine("Point moved from ({0}, {1})  to ({2}, {3})", 
+                this.x, this.y, x, y);
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void MoveBy(double dx, double dy)
+        {
+            Console.Write("Point ({0}, {1}) moved by  ({2}, {3}). ",
+                this.x, this.y, dx, dy);
+            this.x += dx;
+            this.y += dy;
+            Console.WriteLine("New location ({0}, {1})", this.x, this.y);
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rnd = new Random();
+
+            //int r = rnd.Next(0, 10);
+
+            //Stack<int> st = new Stack<int>();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    st.Push(i);
+            //}
+
+            //while (st.Count > 0)
+            //{
+            //    Console.WriteLine(st.Pop());
+            //}
+
+
+            //Process.Start("notepad");
+            //Process[] procs = Process.GetProcesses();
+            //foreach (var item in procs)
+            //{
+            //    Console.WriteLine($"{item.ProcessName} - {item.Id} - {item.PagedMemorySize}");
+            //}
+
+
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(3, 4);
+            Console.WriteLine(p1.DistanceToOrigin());
+            Console.WriteLine(p1.DistanceTo(p2));
+            p1.Print();
+            p2.Print();
+
+            p1.MoveTo(10, 20);
+            p2.MoveBy(4, 5);
+        }
+    }
+}
