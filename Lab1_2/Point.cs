d40c7f58c38378bae08e95a2afcf37d90068b7b5
Horea Oros <horea.oros@gmail.com>
2020-02-27 11:11:46 +0200

    Add Lab1_2

---
+++ Lab1_2/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_2
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            Console.WriteLine("Am creat un obiect de tip Point");
+        }
+
+        public double DistanceToOrigin()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public double DistanceTo(Point p2)
+        {
+            return Math.Sqrt(Math.Pow(x - p2.x, 2) + Math.Pow(y - p2.y, 2));
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void MoveBy(int dx, int dy)
+        {
+            this.x += dx;
+            this.y += dy;
+        }
+    }
+}
---
+++ Lab1_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rnd = new Random();
+            //Console.WriteLine(rnd.Next(0, 10));
+
+            //Stack<int> st = new Stack<int>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    st.Push(i);
+            //}
+            //while (st.Count > 0)
+            //{
+            //    Console.WriteLine(st.Pop());
+            //}
+
+            //Process.Start("notepad");
+
+
+
+            //Process[] procs = Process.GetProcesses();
+            //foreach (var item in procs)
+            //{
+            //    Console.WriteLine($"{item.ProcessName}");
+            //}
+
+
+            Point p1 = new Point(1, 2);
+            Point p2 = new Point(3, 4);
+
+
+            Console.WriteLine(p1.DistanceToOrigin());
+            Console.WriteLine(p2.DistanceToOrigin());
+
+            Console.WriteLine(p1.DistanceTo(p2));
+
+            p1.MoveTo(2, 3);
+            p2.MoveBy(1, 5);
+
+
+            // TODO
+            //p1.Rotate(30);
+        }
+    }
+}
