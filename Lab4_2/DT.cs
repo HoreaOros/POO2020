d176992f2532522d249ebc10feed24946ceec304
Horea Oros <horea.oros@gmail.com>
2020-03-18 12:25:00 +0200

    add DT, TS lab project.

---
+++ Lab4_2/DT.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    public class DT
+    {
+        private int h, mm, ss, zi, luna, an;
+        public DT(): base()
+        {
+            DateTime dt = DateTime.Now;
+
+            h = dt.Hour;
+            mm = dt.Minute;
+            ss = dt.Second;
+            zi = dt.Day;
+            luna = dt.Month;
+            an = dt.Year;
+        }
+        public DT(int an, int luna, int zi, int h, int mm, int ss)
+        {
+            // validarea datelor furnizate
+
+            this.h = h;
+            this.mm = mm;
+            this.ss = ss;
+            this.zi = zi;
+            this.luna = luna;
+            this.an = an;
+        }
+
+        // proprietate readonly
+        public int Year 
+        { 
+            get
+            {
+                return an;
+            }
+            //set
+            //{
+            //    an = value;
+            //}
+        }
+
+        public void AddYears(int y)
+        {
+            an += y;
+        }
+
+        // TODO
+        // proprietati readonly pentru celelalte campuri
+
+        public override string ToString()
+        {
+            return h.ToString() + ":" + mm.ToString() + ":" + ss.ToString() + " " + 
+                zi.ToString() + "/" + luna.ToString() + "/" + an.ToString();
+        }
+    }
+}
---
+++ Lab4_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            //TimeSpan ts = new TimeSpan(1, 5, 4);
+
+            
+            
+            //Console.WriteLine(dt);
+            //Console.WriteLine(ts);
+
+            DT d = new DT(); // ce trebuie sa faca constructorul fara parametri??? i18n
+
+            Console.WriteLine(d);
+
+            DT d2 = new DT(2020, 3, 18, 0, 0, 0);
+
+            Console.WriteLine($"Anul: {d2.Year}");
+
+
+            // operatii pentru DT
+            // diferenta dintre doua date calendaristice
+            // getters(/setters) pentru componentele lui DT
+
+            d2.AddYears(10);
+            Console.WriteLine($"Anul: {d2}");
+
+            // d2.AddMonths(20);// trebuie gasit un raspuns adecvat la modul in care trebuie calculat rezultatul.
+
+            // TODO
+            // adaugati operatii cu tipul de date DT (similare cu cele pe care le are DateTime)
+
+            // TODO
+            // Realizati o implementare proprie pentru TimeSpan (TS) - modelati obiectual conceptul de interval de timp
+            // veti folosi ca si model tipul TimeSpan.
+
+
+
+        }
+    }
+}
