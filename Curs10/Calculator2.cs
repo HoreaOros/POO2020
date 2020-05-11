4d5039a92cf23749ae91fa3cddb2f00f7e00c29e
Rodi Younies <govor_rodica@yahoo.com>
2020-05-11 23:10:46 +0300

    replaced constructor with parser

---
+++ Curs10/Calculator2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Curs10
+{
+    public class Calculator2
+    {
+        private int op1, op2; // operanzii expresiei
+        private char operatie;
+        public int Value { get; private set; }
+
+        public ICalculator c;
+        
+        //TODO
+        // eliminam constructorul care primeste string iar parsingul epxresiei se va face in afara
+        // clasei Calcualtor2
+        // ramane doar construtorul cu 3 parametri --> public Calculator2(int op1, int op2, ICalculator c)
+              
+       
+        public Calculator2(int op1, int op2, ICalculator c)
+        {
+            this.op1 = op1;
+            this.op2 = op2;
+            this.c = c;
+        }
+        public void Evaluate()
+        {
+            Value = c.Operation(op1, op2);
+        }
+    }
+}
