d714c244d7d1685ff844f11455078b018535e294
Horea Oros <horea.oros@gmail.com>
2020-05-13 11:27:27 +0300

    ADD BItArray project, AtomPlant

---
+++ AtomPlant/AtomPlant/State.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomPlant
+{
+    class State
+    {
+        private string name;
+        AtomPlant ap;
+        public State(string s, AtomPlant ap)
+        {
+            name = s;
+            this.ap = ap;
+            this.ap.Explosion += new EventHandler<MEventArgs>(ap_Explosion);
+        }
+
+        void ap_Explosion(object sender, MEventArgs e)
+        {
+            Console.WriteLine("{0} received a notification of the explosion", name);
+            Console.WriteLine("Core temperature: {0}", e.Temperature);
+        }
+        public string Name
+        {
+            get
+            {   
+                return name;
+            }
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray b1 = new BitArray(15);
+            BitArray b2 = new BitArray(new byte[] {0xff, 0xf, 0x1f});
+            for (int i = 0; i < b2.Length; i++)
+            {
+                int v = b2[i] ? 1 : 0;
+                Console.Write($"{v} ");
+            }
+
+            Console.WriteLine();
+
+
+            foreach (var item in b2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            // aici testez setterul indexatorului
+            b1[3] = true;
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+        }
+    }
+}
