01549b1c3846e762ea001763348a321088ede9be
Horea Oros <horea.oros@gmail.com>
2020-05-14 12:18:51 +0300

    add SuperStack class that implements IStack

--- Curs11/Program.cs
+++ Curs11/Program.cs
@@ -11,28 +11,57 @@ namespace Curs11
     {
         static void Main(string[] args)
         {
-            MyStackOfInt st = new MyStackOfInt();
+            Console.WriteLine("MyStack of int demo");
+            MyStack<int> st = new MyStack<int>();
 
             st.Push(1);
             st.Push(2);
             st.Push(3);
 
-
-            Console.WriteLine("Elementeul din varful stivei este: {0}", st.Peek());
-
-            while (!st.Empty)
-            {
-                Console.WriteLine(st.Pop());
-            }
+            
 
 
-            MyStackOfString stStr = new MyStackOfString();
+
+            Console.WriteLine("Elementul din varful stivei este: {0}", st.Peek());
+
+
+            DumpStack(st);
+
+
+
+            Console.WriteLine("MyStack of string demo");
+            MyStack<string> stStr = new MyStack<string>();
 
             stStr.Push("Hello");
             stStr.Push("World");
 
 
+            DumpStack(stStr);
 
+
+
+            Console.WriteLine("SuperStack of int demo");
+
+            SuperStack<int> ss = new SuperStack<int>();
+
+            ss.Push(4);
+            ss.Push(3);
+            ss.Push(2);
+            ss.Push(1);
+
+
+            Console.WriteLine(ss.Peek());
+
+            DumpStack(ss);
+
+        }
+
+        public static void DumpStack<T>(IStack<T> st)
+        {
+            while (!st.Empty)
+            {
+                Console.WriteLine(st.Pop());
+            }
         }
     }
 }
