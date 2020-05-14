68f85eb1fc4e8199c54035afe9c8d78ec22c7755
Horea Oros <horea.oros@gmail.com>
2020-05-14 11:22:27 +0300

    add stack classes for int and string

---
+++ Curs11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStackOfInt st = new MyStackOfInt();
+
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+
+
+            Console.WriteLine("Elementeul din varful stivei este: {0}", st.Peek());
+
+            while (!st.Empty)
+            {
+                Console.WriteLine(st.Pop());
+            }
+
+
+            MyStackOfString stStr = new MyStackOfString();
+
+            stStr.Push("Hello");
+            stStr.Push("World");
+
+
+
+        }
+    }
+}
---
+++ Curs11/StackFullException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Curs11
+{
+    [Serializable]
+    internal class StackFullException : Exception
+    {
+        public StackFullException()
+        {
+        }
+
+        public StackFullException(string message) : base(message)
+        {
+        }
+
+        public StackFullException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StackFullException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
